35302f799e31704eadd197aa548a87e8949f6702
Daniel Schroeder <schrsistemas@gmail.com>
2026-09-18 18:35:28 -0300

    fix(device): use sync repository in atomic event ingest

--- backend/src/services/device.event.service.js
+++ backend/src/services/device.event.service.js
@@ -1,6 +1,6 @@
 const crypto = require('node:crypto');
 const db = require('./db.firebird.service');
-const syncService = require('./sync.service');
+const syncRepository = require('../repository/sync.repository');
 
 const ALLOWED_TYPES = new Set(['arduino', 'raspberry-pi', 'pic', 'android', 'ios', 'delphi', 'simulator']);
 
