4057b467617be9031c2bd77d18958b5f77ab935a
Daniel Schroeder <schrsistemas@gmail.com>
2026-09-19 18:08:16 -0300

    refactor(data): remove obsolete firebird service wrapper

--- backend/src/services/db.firebird.service.js
+++
@@ -1,9 +0,0 @@
-const connection = require('../db/firebird.connection');
-
-class SqlDatabaseService {
-  constructor(adapter = connection) { this.adapter = adapter; }
-  query(sql, params = []) { return this.adapter.query(sql, params); }
-  execute(sql, params = []) { return this.adapter.execute(sql, params); }
-  withTransaction(work) { return this.adapter.withTransaction(work); }
-}
-module.exports = new SqlDatabaseService();
